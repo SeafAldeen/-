2a01f87f1eacc001645f0d654efd2c90e8ec9525
SeafAldeen <95848004+SeafAldeen@users.noreply.github.com>
2022-08-14 16:36:12 +0200

    Create LabelTest.java

---
+++ LabelTest.java
@@ -0,0 +1,20 @@
+
+
+import java.awt.*;
+import javax.swing.*;
+public class LabelTest extends JFrame {
+public LabelTest( ) {
+super ("Label Test");
+Container container = getContentPane ( );
+container.setLayout(new FlowLayout( ));
+JLabel label1 = new JLabel ("This is a label");
+container.add(label1);
+Icon image = new ImageIcon ("image.gif");
+JLabel label2 = new JLabel (image);
+container.add (label2);
+JLabel label3 = new JLabel ( );
+Label3.setText ("This is another label");
+container.add (label3);
+setSize (200, 100);
+setVisible (true); 
+}
